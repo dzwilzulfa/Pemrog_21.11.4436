6f428350830bf5ca5cfeed23f71f77207a70e468
MN08 <mnurhuda009@gmail.com>
2022-07-16 11:24:22 +0700

    responsi

---
+++ resnponsipemrog_4436/resnponsipemrog_4436/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resnponsipemrog_4436
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public float IPK { get; set; }
+    }
+}
